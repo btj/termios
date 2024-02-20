ae4a1112a1ea37a6280148d22d01ac500228d052
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-02-20 15:06:20 +0100

    The library works on Windows

--- Makefile
+++ Makefile
@@ -4,6 +4,34 @@ ifndef JAVA_HOME
   $(error Please set JAVA_HOME to the path to your JDK)
 endif
 
+ifdef WINDIR
+  CFLAGS=
+  JNI_INCLUDE_DIR=win32
+  LIBNAME=io_github_btj_termios.dll
+  SHARED_FLAG=-shared
+  LINKFLAGS=-Wl,--add-stdcall-alias
+  PATHSEP=;
+else
+  ifdef MACHTYPE
+    JNI_INCLUDE_DIR=darwin
+	LIBNAME=libio_github_btj_termios.dylib
+	SHARED_FLAG=-dynamiclib
+	LINKFLAGS=-lc
+  else
+    JNI_INCLUDE_DIR=linux
+	LIBNAME=libio_github_btj_termios.so
+	SHARED_FLAG=-shared -fPIC
+	LINKFLAGS=-lc
+  endif
+  CFLAGS=-fPIC
+  PATHSEP=:
+endif
+
+jar: tests _build/main/io.github.btj.termios.jar
+
+_build/main/io.github.btj.termios.jar: termios
+	jar cf _build/main/io.github.btj.termios.jar -C _build/main/classes .
+
 tests: _build/test/classes/TerminalRawInputTest.class _build/test/classes/TerminalTest.class
 
 _build/test/classes/TerminalTest.class: termios src/test/java/TerminalTest.java
@@ -13,19 +41,19 @@ _build/test/classes/TerminalRawInputTest.class: termios src/test/java/TerminalRa
 	javac -d _build/test/classes -cp _build/main/classes src/test/java/TerminalRawInputTest.java
 
 .PHONY: termios
-termios: _build/main/classes/io/github/btj/termios/Terminal.class _build/main/classes/libio_github_btj_termios.dylib
+termios: _build/main/classes/io/github/btj/termios/Terminal.class _build/main/classes/${LIBNAME}
 
-_build/main/classes/libio_github_btj_termios.dylib: _build/main/io_github_btj_termios_Terminal.o
-	gcc -dynamiclib -o _build/main/classes/libio_github_btj_termios.dylib _build/main/io_github_btj_termios_Terminal.o -lc
+_build/main/classes/${LIBNAME}: _build/main/io_github_btj_termios_Terminal.o
+	gcc ${SHARED_FLAG} -o _build/main/classes/${LIBNAME} _build/main/io_github_btj_termios_Terminal.o ${LINKFLAGS}
 
 _build/main/io_github_btj_termios_Terminal.o: _build/main/include/io_github_btj_termios_Terminal.h src/main/c/io_github_btj_termios_Terminal.c
-	gcc -c -fPIC -I _build/main/include -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin src/main/c/io_github_btj_termios_Terminal.c -o _build/main/io_github_btj_termios_Terminal.o
+	gcc -c $(CFLAGS) -I _build/main/include "-I${JAVA_HOME}/include" "-I${JAVA_HOME}/include/${JNI_INCLUDE_DIR}" src/main/c/io_github_btj_termios_Terminal.c -o _build/main/io_github_btj_termios_Terminal.o
 
 _build/main/classes/io/github/btj/termios/Terminal.class _build/main/include/io_github_btj_termios_Terminal.h: src/main/java/io/github/btj/termios/Terminal.java
 	javac -h _build/main/include -d _build/main/classes src/main/java/io/github/btj/termios/Terminal.java
 
 test_raw_input: _build/test/classes/TerminalRawInputTest.class
-	java -cp _build/main/classes:_build/test/classes TerminalRawInputTest
+	java -cp "_build/main/classes${PATHSEP}_build/test/classes" TerminalRawInputTest
 
 test: _build/test/classes/TerminalTest.class
-	java -cp _build/main/classes:_build/test/classes TerminalTest
+	java -cp "_build/main/classes${PATHSEP}_build/test/classes" TerminalTest
--- src/main/c/io_github_btj_termios_Terminal.c
+++ src/main/c/io_github_btj_termios_Terminal.c
@@ -1,39 +1,69 @@
-#include "io_github_btj_termios_Terminal.h"
 #include <stdio.h>
-#include <termios.h>
+#include "io_github_btj_termios_Terminal.h"
 
-struct termios original_term;
+#ifdef _WIN32
+    #include <windows.h>
 
-JNIEXPORT void JNICALL Java_io_github_btj_termios_Terminal_enterRawInputMode(JNIEnv *env, jclass class_) {
-    tcgetattr(fileno(stdin), &original_term);
+    DWORD oldConsoleMode;
+    HANDLE hStdin;
 
-    struct termios raw;
-    tcgetattr(fileno(stdin), &raw);
+    JNIEXPORT void JNICALL Java_io_github_btj_termios_Terminal_enterRawInputMode(JNIEnv *env, jclass class_) {
+        hStdin = GetStdHandle(STD_INPUT_HANDLE);
+        if (hStdin == INVALID_HANDLE_VALUE)
+            (*env)->FatalError(env, "Couldn't get the stdin handle");
 
-    /* input modes - clear indicated ones giving: no break, no CR to NL,
-       no parity check, no strip char, no start/stop output (sic) control */
-    //raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    
-    raw.c_iflag &= ~ICRNL; // Without this line, Return key presses are reported as LF; with it, they are reported as CR.
-    raw.c_iflag &= ~BRKINT; // Without this line, Ctrl+C is processed by the terminal itself and not delivered to the application.
-    raw.c_iflag &= ~IXON; // Without this line, Ctrl+S is processed by the terminal itself and not delivered to the application.
+        if (!GetConsoleMode(hStdin, &oldConsoleMode) )
+            (*env)->FatalError(env, "Couldn't get the console mode. Is stdin not a console?");
 
-    /* output modes - clear giving: no post processing such as NL to CR+NL */
-    //raw.c_oflag &= ~(OPOST);
+        if (!SetConsoleMode(hStdin, ENABLE_VIRTUAL_TERMINAL_INPUT))
+            (*env)->FatalError(env, "Couldn't set the console mode. Is stdin not a console?");
+    }
 
-    /* control modes - set 8 bit chars */
-    //raw.c_cflag |= (CS8);
+    JNIEXPORT void JNICALL Java_io_github_btj_termios_Terminal_leaveRawInputMode(JNIEnv *env, jclass class_) {
+        if (!SetConsoleMode(hStdin, oldConsoleMode))
+            (*env)->FatalError(env, "Couldn't set the console mode. Is stdin not a console?");
+    }
 
-    /* local modes - clear giving: echoing off, canonical off (no erase with
-       backspace, ^U,...),  no extended functions, no signal chars (^Z,^C) */
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+#else
 
-    /* control chars - set return condition: min number of bytes and timer */
-    raw.c_cc[VMIN] = 1; raw.c_cc[VTIME] = 0; /* immediate - anything       */
+    #include <termios.h>
 
-    tcsetattr(fileno(stdin), TCSANOW, &raw);
-}
+    struct termios original_term;
 
-JNIEXPORT void JNICALL Java_io_github_btj_termios_Terminal_leaveRawInputMode(JNIEnv *env, jclass class_) {
-    tcsetattr(fileno(stdin), TCSANOW, &original_term);
-}
+    JNIEXPORT void JNICALL Java_io_github_btj_termios_Terminal_enterRawInputMode(JNIEnv *env, jclass class_) {
+        if (tcgetattr(fileno(stdin), &original_term))
+            (*env)->FatalError(env, "Could not get the terminal attributes. Is stdin not a terminal?");
+
+        struct termios raw = original_term;
+
+        /* input modes - clear indicated ones giving: no break, no CR to NL,
+        no parity check, no strip char, no start/stop output (sic) control */
+        //raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        
+        raw.c_iflag &= ~ICRNL; // Without this line, Return key presses are reported as LF; with it, they are reported as CR.
+        raw.c_iflag &= ~BRKINT; // Without this line, Ctrl+C is processed by the terminal itself and not delivered to the application.
+        raw.c_iflag &= ~IXON; // Without this line, Ctrl+S is processed by the terminal itself and not delivered to the application.
+
+        /* output modes - clear giving: no post processing such as NL to CR+NL */
+        //raw.c_oflag &= ~(OPOST);
+
+        /* control modes - set 8 bit chars */
+        //raw.c_cflag |= (CS8);
+
+        /* local modes - clear giving: echoing off, canonical off (no erase with
+        backspace, ^U,...),  no extended functions, no signal chars (^Z,^C) */
+        raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+        /* control chars - set return condition: min number of bytes and timer */
+        raw.c_cc[VMIN] = 1; raw.c_cc[VTIME] = 0; /* immediate - anything       */
+
+        if (tcsetattr(fileno(stdin), TCSANOW, &raw))
+            (*env)->FatalError(env, "Could not set the terminal attributes. Is stdin not a terminal?");
+    }
+
+    JNIEXPORT void JNICALL Java_io_github_btj_termios_Terminal_leaveRawInputMode(JNIEnv *env, jclass class_) {
+        if (tcsetattr(fileno(stdin), TCSANOW, &original_term))
+            (*env)->FatalError(env, "Could not set the terminal attributes. Is stdin not a terminal?");
+    }
+
+#endif
